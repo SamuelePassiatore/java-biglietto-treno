845952ba5306fda857dec06fec3e3500ff5ff699
SamuelePassiatore <semmisno2@protonmail.com>
2023-05-04 15:39:34 +0200

    array foods ranking

--- src/org/lessons/java/CibiPreferiti.java
+++ src/org/lessons/java/CibiPreferiti.java
@@ -2,6 +2,20 @@ package org.lessons.java;
 
 public class CibiPreferiti {
 	public static void main(String[] args){
+		String[] cibiPreferiti = {"Pizza", "Pasta al sugo", "Salmone", "Polenta", "Gelato al cioccolato", "Gesù"};
 		
+		// Ranking length
+        System.out.println("Lunghezza della classifica: " + cibiPreferiti.length);
+        
+        // Ranking top
+        System.out.println("Cibo top: " + cibiPreferiti[0]);
+        
+        // Ranking bottom
+        System.out.println("Cibo preferito ma non troppo: " + cibiPreferiti[cibiPreferiti.length - 1]);
+        
+        // Ranking middle
+        int posizioneMedia = cibiPreferiti.length / 2;
+        System.out.println("Il cibo di mezza classifica è: " + cibiPreferiti[posizioneMedia]);
+     
 	}
 }
