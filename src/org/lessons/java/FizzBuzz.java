fae2846d7eef9251077a4d6b679ab29e3f8ee7c0
SamuelePassiatore <semmisno2@protonmail.com>
2023-05-04 17:12:29 +0200

    added scanner with number user and for loop

--- src/org/lessons/java/FizzBuzz.java
+++ src/org/lessons/java/FizzBuzz.java
@@ -1,8 +1,27 @@
 package org.lessons.java;
 
+import java.util.Scanner;
+
 public class FizzBuzz {
 	public static void main(String[] args){
-		
-     
+		 Scanner scanner = new Scanner(System.in);
+
+	        System.out.print("Inserisci il numero di valori da valutare: ");
+	        int number = scanner.nextInt();
+
+	        // For loop
+	        for (int i = 1; i <= number; i++) {
+	            if (i % 3 == 0 && i % 5 == 0) {
+	                System.out.println("FizzBuzz");
+	            } else if (i % 3 == 0) {
+	                System.out.println("Fizz");
+	            } else if (i % 5 == 0) {
+	                System.out.println("Buzz");
+	            } else {
+	                System.out.println(i);
+	            }
+	        }
+
+	        scanner.close();
 	}
 }
