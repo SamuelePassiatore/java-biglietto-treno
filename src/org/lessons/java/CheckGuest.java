f773d73bace58768f13b81cb437684ddc5d4642d
SamuelePassiatore <semmisno2@protonmail.com>
2023-05-04 17:25:30 +0200

    made check guest of scanner with for loop

--- src/org/lessons/java/CheckGuest.java
+++ src/org/lessons/java/CheckGuest.java
@@ -1,8 +1,33 @@
 package org.lessons.java;
 
+import java.util.Scanner;
 
 public class CheckGuest {
 	public static void main(String[] args){
-		
+		  // Data
+		  String[] guestsList = {"Dua Lipa", "Paris Hilton", "Manuel Agnelli", "J-Ax", "Francesco Totti", "Ilary Blasi", "Bebe Vio", "Luis", "Pardis Zarei", "Martina Maccherone", "Rachel Zeilic"};
+		  
+		  // Scanner
+		  Scanner scanner = new Scanner(System.in);
+	        System.out.println("Inserisci il tuo nome: ");
+	        String name = scanner.nextLine();
+	        
+	        // Is guest boolean and for loop
+	        boolean isGuest = false;
+	        for (int i = 0; i < guestsList.length; i++) {
+	        	if (guestsList[i].equals(name)) {
+	                isGuest = true;
+	                break;
+	            }
+	        }
+	        
+	        // Response to user
+	        if (isGuest) {
+	            System.out.println("Benvenuto alla festa!");
+	        } else {
+	            System.out.println("Spiacenti, il tuo nome non è sulla lista degli invitati.");
+	        }
+	        
+	        scanner.close();
 	}
 }
