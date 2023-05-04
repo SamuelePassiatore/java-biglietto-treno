526190e4a7ef9cd7f8cfd91af96a9b35c55293c9
SamuelePassiatore <semmisno2@protonmail.com>
2023-05-04 15:08:20 +0200

    calculate ticket price with discounts

--- src/org/lessons/java/CalcolaBiglietto.java
+++ src/org/lessons/java/CalcolaBiglietto.java
@@ -1,18 +1,33 @@
 package org.lessons.java;
 
 import java.util.Scanner;
+import java.text.DecimalFormat;
 
 public class CalcolaBiglietto {
 	public static void main(String[] args){
 		Scanner scanner = new Scanner(System.in);
+		DecimalFormat df = new DecimalFormat("#.##");
 		
+		// Kms number
 		System.out.println("Inserisci il numero di chilometri che vuoi percorrere: ");
         int km = scanner.nextInt();
         
+        // Age of passenger
         System.out.println("Inserisci l'età del passeggero: ");
         int age = scanner.nextInt();
-
         
+        // Full price ticket
+        double fullPrice = km * 0.21;
+
+        // Ticket discounts
+        if (age < 18) {
+            fullPrice *= 0.8; 
+        } else if (age >= 65) {
+            fullPrice *= 0.6;  
+        }
+
+        // Result
+        System.out.println("Il prezzo del biglietto è: " + df.format(fullPrice) + " €");
 
         scanner.close();
 	}
