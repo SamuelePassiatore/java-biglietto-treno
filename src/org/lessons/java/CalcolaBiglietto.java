7ad8b3c2ccc184eaf1e88d98ed8375827991d439
SamuelePassiatore <semmisno2@protonmail.com>
2023-05-04 14:51:18 +0200

    added scanner questions

--- src/org/lessons/java/CalcolaBiglietto.java
+++ src/org/lessons/java/CalcolaBiglietto.java
@@ -1,7 +1,19 @@
 package org.lessons.java;
 
+import java.util.Scanner;
+
 public class CalcolaBiglietto {
 	public static void main(String[] args){
+		Scanner scanner = new Scanner(System.in);
 		
+		System.out.println("Inserisci il numero di chilometri che vuoi percorrere: ");
+        int km = scanner.nextInt();
+        
+        System.out.println("Inserisci l'età del passeggero: ");
+        int age = scanner.nextInt();
+
+        
+
+        scanner.close();
 	}
 }
